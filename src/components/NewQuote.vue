3c3eedd15309cd668d8769f2f8239fbc7e8a33ec
Brian Nelson <ladofmusi@outlook.com>
2019-10-28 13:14:46 -0600

    first commit, working on progress bar right now

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <form>
+        <div class="col-sm-8">
+        <label>quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+    </div>
+    <div class="col-sm-8">
+        <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+    </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data: function()  {
+        return{
+            quote: ''
+        };
+    },
+    methods: {
+        createNew() {
+            this.$emit('quoteAdded', this.quote)
+            this.quote = '';
+        }
+    }
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-6">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  font-size: 24px;
+  color: gray;
+}
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+  background-color: lightcoral;
+}
+</style>
